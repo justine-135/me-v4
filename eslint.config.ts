e56c6eb67268d27a3dc7a0f9f16e7b1a546b3012
justine-135 <justineupano87@gmail.com>
2025-08-09 13:14:23 +0800

    init files

---
+++ eslint.config.ts
@@ -0,0 +1,57 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      react,
+      prettier: prettierPlugin,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      // React rules
+      "react/react-in-jsx-scope": "off", // Not needed for React 17+
+      "react/prop-types": "off", // Using TypeScript instead
+
+      // TypeScript rules
+      "@typescript-eslint/no-unused-vars": ["warn"],
+
+      // Prettier rules
+      "prettier/prettier": [
+        "error",
+        {
+          semi: false,
+          singleQuote: true,
+          trailingComma: "es5",
+          tabWidth: 2,
+          printWidth: 100,
+          endOfLine: "auto",
+        },
+      ],
+    },
+  }
+);
