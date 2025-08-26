70f511ada4c9f0e6434c1a3b62259fce3cfdc3a0
justine-135 <justineupano87@gmail.com>
2025-08-26 23:40:39 +0800

    implement formspree for contact form submit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { CustomChakraProvider } from './providers/CustomChakraProvider.tsx'
 import { BrowserRouter as Router } from 'react-router'
+import { Toaster } from './components/ui/toaster.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CustomChakraProvider>
       <Router>
+        <Toaster />
         <App />
       </Router>
     </CustomChakraProvider>
--- src/pages/ContactPage/FormCard.tsx
+++ src/pages/ContactPage/FormCard.tsx
@@ -1,33 +1,66 @@
 import CardCustom from '@/components/CardCustom'
+import { toaster } from '@/components/ui/toaster'
 import { Button, Field, Fieldset, Input, Stack, Textarea } from '@chakra-ui/react'
+import { useForm } from '@formspree/react'
+import { useEffect } from 'react'
+
+const FORM_SPREE = import.meta.env.VITE_APP_FORM_SPREE
 
 export const FormCard = () => {
+  const [state, handleSubmit] = useForm(FORM_SPREE)
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    handleSubmit(e)
+  }
+
+  useEffect(() => {
+    if (state.succeeded) {
+      queueMicrotask(() =>
+        toaster.create({
+          description: 'Email submitted',
+          type: 'success',
+        })
+      )
+    }
+
+    if (state.errors) {
+      queueMicrotask(() =>
+        toaster.create({
+          description: 'Not submitted',
+          type: 'error',
+        })
+      )
+    }
+  }, [state.succeeded, state.errors])
+
   return (
     <CardCustom cardTitle="Contact details" as="section">
       <Fieldset.Root>
-        <Stack>
-          <Fieldset.HelperText>Please provide your contact details below.</Fieldset.HelperText>
-        </Stack>
-        <Fieldset.Content>
-          <Field.Root>
-            <Field.Label>Name</Field.Label>
-            <Input name="name" />
-          </Field.Root>
+        <Fieldset.HelperText>Please provide your contact details below.</Fieldset.HelperText>
+        <form onSubmit={(e) => handleOnSubmit(e)}>
+          <Stack>
+            <Fieldset.Content>
+              <Field.Root>
+                <Field.Label>Email address</Field.Label>
+                <Input name="email" type="email" />
+              </Field.Root>
 
-          <Field.Root>
-            <Field.Label>Email address</Field.Label>
-            <Input name="email" type="email" />
-          </Field.Root>
+              <Field.Root>
+                <Field.Label>Subject</Field.Label>
+                <Input name="subject" />
+              </Field.Root>
 
-          <Field.Root>
-            <Field.Label>Body</Field.Label>
-            <Textarea rows={4} />
-          </Field.Root>
-        </Fieldset.Content>
+              <Field.Root>
+                <Field.Label>Message</Field.Label>
+                <Textarea name="message" rows={4} />
+              </Field.Root>
+            </Fieldset.Content>
 
-        <Button type="submit" alignSelf="flex-end">
-          Submit
-        </Button>
+            <Button type="submit" alignSelf="flex-end" loading={state.submitting}>
+              Submit
+            </Button>
+          </Stack>
+        </form>
       </Fieldset.Root>
     </CardCustom>
   )
