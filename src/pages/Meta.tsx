61792a0244c7ca052ff3b54d4ad69c36dca2aa49
justine-135 <justineupano87@gmail.com>
2025-08-27 12:34:08 +0800

    fix image url meta tag

--- src/pages/Meta.tsx
+++ src/pages/Meta.tsx
@@ -5,24 +5,26 @@ export interface MetaProps {
   image?: string
 }
 
+const IMAGE_URL = import.meta.env.VITE_APP_IMAGE_URL
+
 export default function Meta({ title, description, url, image }: MetaProps) {
-  const defaultTitle = title || 'Portfolio'
+  const defaultTitle = `Justine Upano | ${title || 'Portfolio'}`
   const defaultDescription = description || 'Welcome to my portfolio page'
-  const defaultImage = '/home.png'
+  const defaultImage = `${IMAGE_URL}${image || '/home.png'}`
 
   return (
     <head>
-      <title>{`Justine Upano | ${defaultTitle}`}</title>
+      <title>{defaultTitle}</title>
       <meta name="description" content={defaultDescription} />
 
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={defaultTitle} />
       <meta property="og:description" content={defaultDescription} />
       <meta property="og:type" content="website" />
       {url && <meta property="og:url" content={url} />}
       <meta property="og:image" content={image || defaultImage} />
 
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
+      <meta name="twitter:title" content={defaultTitle} />
       <meta name="twitter:description" content={defaultDescription} />
       <meta name="twitter:image" content={image || defaultImage} />
     </head>
