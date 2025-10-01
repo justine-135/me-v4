0e14f5a777fd1e16c1a7b98bc067c50b9b275839
justine-135 <justineupano87@gmail.com>
2025-10-01 12:25:43 +0800

    add scrollable sidebar, remove theme switcher on mobile

--- src/components/Sidebar/SidebarContent.tsx
+++ src/components/Sidebar/SidebarContent.tsx
@@ -3,7 +3,7 @@ import type { IDev } from '@/types/Dev'
 import { fetcher } from '@/util'
 import useSWR from 'swr'
 import AvatarSection from './AvatarSection'
-import { Stack, Separator } from '@chakra-ui/react'
+import { Stack, Separator, Box } from '@chakra-ui/react'
 import { Typography } from '../Typography'
 import CurrentlySection from './CurrentlySection'
 import NavLinks from './NavLinks'
@@ -13,29 +13,35 @@ export default function SidebarContent() {
   const { data, isLoading } = useSWR<IDev>(DEV_JSON_PATH, fetcher)
 
   return (
-    <Stack gap={4}>
-      <AvatarSection name={data?.name} subtitle={data?.subtitle} isLoading={isLoading} />
-      <Separator />
-      <Typography fontWeight={500} fontSize="xs">
-        NAVIGATION
-      </Typography>
-      <NavLinks />
-      <Separator />
-      <Typography fontWeight={500} fontSize="xs">
-        CURRENTLY
-      </Typography>
-      <CurrentlySection
-        working_on={data?.working_on}
-        learning_on={data?.learning_on}
-        time={data?.time}
-        location={data?.location}
-        isLoading={isLoading}
-      />
-      <Separator />
-      <Typography fontWeight={500} fontSize="xs">
-        CONNECT
-      </Typography>
-      <SocialsSection socials={data?.socials} isLoading={isLoading} />
-    </Stack>
+    <Box
+      scrollbar={['hidden', 'auto']}
+      overflowY={['hidden', 'auto']}
+      maxH={{ sm: 'calc(89vh - 20px)' }}
+    >
+      <Stack gap={4}>
+        <AvatarSection name={data?.name} subtitle={data?.subtitle} isLoading={isLoading} />
+        <Separator />
+        <Typography fontWeight={500} fontSize="xs">
+          NAVIGATION
+        </Typography>
+        <NavLinks />
+        <Separator />
+        <Typography fontWeight={500} fontSize="xs">
+          CURRENTLY
+        </Typography>
+        <CurrentlySection
+          working_on={data?.working_on}
+          learning_on={data?.learning_on}
+          time={data?.time}
+          location={data?.location}
+          isLoading={isLoading}
+        />
+        <Separator />
+        <Typography fontWeight={500} fontSize="xs">
+          CONNECT
+        </Typography>
+        <SocialsSection socials={data?.socials} isLoading={isLoading} />
+      </Stack>
+    </Box>
   )
 }
--- src/components/Sidebar/SidebarDrawer.tsx
+++ src/components/Sidebar/SidebarDrawer.tsx
@@ -1,6 +1,5 @@
 import { Button, CloseButton, Drawer, Portal } from '@chakra-ui/react'
 import { BiMenuAltLeft } from 'react-icons/bi'
-import { ToggleThemeButton } from '../Button/ToggleThemeButton'
 import SidebarContent from './SidebarContent'
 import { useState } from 'react'
 import { SidebarDrawerContext } from '@/context/SidebarDrawerContext'
@@ -19,14 +18,11 @@ export const SidebarDrawer = () => {
         <Drawer.Backdrop />
         <Drawer.Positioner>
           <Drawer.Content>
-            <Drawer.Body py={4}>
+            <Drawer.Body py={6}>
               <SidebarDrawerContext.Provider value={{ open, setOpen }}>
                 <SidebarContent />
               </SidebarDrawerContext.Provider>
             </Drawer.Body>
-            <Drawer.Footer>
-              <ToggleThemeButton />
-            </Drawer.Footer>
             <Drawer.CloseTrigger asChild>
               <CloseButton size="sm" />
             </Drawer.CloseTrigger>
